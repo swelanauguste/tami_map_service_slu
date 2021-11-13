ceafb4dc4664e3f671d707c3baef4f855e4ab647
swelanauguste <swelanauguste@gmail.com>
2021-11-13 03:27:07 -0400

    address

--- app/customers/models.py
+++ app/customers/models.py
@@ -8,6 +8,7 @@ User = settings.AUTH_USER_MODEL
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=10, null=True)
+    address = models.TextField(blank=True, null=True)
     
     def __str__(self):
         return self.user.email
